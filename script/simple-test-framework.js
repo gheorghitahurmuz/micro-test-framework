779371b2ed01802818600c78a6934e3deaa09ac3
Gheorghita Hurmuz <hurmuz.gheorghita@pm.me>
2023-02-25 16:39:43 +0200

    init

---
+++ script/simple-test-framework.js
@@ -0,0 +1,21 @@
+// Idea from Kent C Dodds
+
+async function test(title, callback) {
+  try {
+    await callback();
+    console.log(`✓ ${title}`);
+  } catch (error) {
+    console.error(`✕ ${title}`);
+    console.error(error);
+  }
+}
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+  };
+}
